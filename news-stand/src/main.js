1d56ef6ee3400665719a6546dda4d6f80aa3d675
arty0928 <arty0928@konkuk.ac.kr>
2026-01-05 17:51:41 +0900

    refactor: latestCardNews 파일 분리 및 폴더 구조 리팩토링

---
+++ news-stand/src/main.js
@@ -0,0 +1,14 @@
+import { renderHeader } from './features/header/Header.js';
+import { renderLatestNews } from './features/latestNews/index.js';
+import './styles/global.css';
+
+async function init() {
+  try {
+    renderHeader();
+    await renderLatestNews();
+  } catch (error) {
+    console.error('앱 초기화 실패:', error);
+  }
+}
+
+init();
